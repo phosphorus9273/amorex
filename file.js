a122288d1ec28bb1165438c1afc85c29764ead7c
Notwz <129885396+Notwz@users.noreply.github.com>
2024-10-14 22:38:08 -0400

    Update file.js
    unknown unknown
    ss

--- file.js
+++ file.js
@@ -1,4 +1,4 @@
 module.exports = {
-  serverOneABC = 'amorex.onrender.com',
+  serverOneABC = 'amo-sv-m-us.onrender.com',
   serverTwoABC = 'server01.onrender.com',
   serverThreeABC = 'server02.onrender.com',
