8cba2b1e2a311cf4c1e26a8d99ac1f21bac59231
Notwz <129885396+Notwz@users.noreply.github.com>
2024-10-14 22:43:56 -0400

    Update app.js
    213312321

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const  { serverOneABC, serverTwoABC, serverThreeABC } = require('file.js');
   //         changelog[0] = changelog[0].split(":").map((line) => line.trim());
   //         document.getElementById("patchNotes").innerHTML += `<div><b>${changelog[0][0].slice(1).trim()}</b>: ${changelog[0].slice(1).join(":") || "Update lol"}<ul>${changelog.slice(1).map((line) => `<li>${line.slice(1).trim()}</li>`).join("")}</ul><hr></div>`;
   //     }
-  // });//test
+  // });//test222
 
   fetch("changelog.html", { cache: "no-cache" }).then(
     async (ChangelogsHTMLFile) => {
