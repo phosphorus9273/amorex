516d6f1d0d3a3ef96a14ef214d98496f81cdefa9
Notwz <129885396+Notwz@users.noreply.github.com>
2024-10-14 22:50:53 -0400

    Update servereing.js
    12312321321wqafragrshdh1231312321fafasfsafsa

--- servereing.js
+++ servereing.js
@@ -2,5 +2,5 @@ const servereing = {
   serverOneABC = 'amo-sv-m-us.onrender.com',
   serverTwoABC = 'server01.onrender.com',
   serverThreeABC = 'server02.onrender.com',
-},
-module.exports = { servereing }
+}
+export { servereing }
